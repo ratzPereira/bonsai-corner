d57c37f8caea87a71558569c76090db90ca4a855
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-08-11 10:27:41 +0000

    added test for swagger, added cors support

---
+++ src/main/java/com/ratz/bonsaicorner/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.ratz.bonsaicorner.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Value("${cors.originPatterns:default}")
+  private String corsOriginPatterns = "";
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+
+    String[] allowedOrigins = corsOriginPatterns.split(",");
+
+    registry.addMapping("/**")
+        //.allowedMethods("GET", "POST", "PUT")
+        .allowedMethods("*")
+        .allowedOrigins(allowedOrigins)
+        .allowCredentials(true);
+  }
+}
