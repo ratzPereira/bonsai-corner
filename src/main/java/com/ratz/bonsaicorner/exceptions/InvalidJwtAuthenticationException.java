f3ae5136c1a174fc0ff935f4c8af57d87ad293a4
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-08-23 23:05:00 +0000

    now jwt erros throw 401 and not 500

--- src/main/java/com/ratz/bonsaicorner/exceptions/InvalidJwtAuthenticationException.java
+++ src/main/java/com/ratz/bonsaicorner/exceptions/InvalidJwtAuthenticationException.java
@@ -4,10 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class InvalidJwtAuthenticationException extends AuthenticationException {
 
-  public InvalidJwtAuthenticationException(String ex) {
-    super(ex);
-  }
+    public InvalidJwtAuthenticationException(String ex) {
+        super(ex);
+    }
 }
